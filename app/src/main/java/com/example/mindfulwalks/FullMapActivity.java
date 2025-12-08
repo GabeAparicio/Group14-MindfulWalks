d0abf22182b98500034e9260886094189a452ee7
Wassn23 <wassn.hashim@gmail.com>
2025-12-07 21:06:19 -0500

    updated some maps feature

---
+++ app/src/main/java/com/example/mindfulwalks/FullMapActivity.java
@@ -0,0 +1,71 @@
+package com.example.mindfulwalks;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class FullMapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private double latitude;
+    private double longitude;
+    private String title;
+    private String address;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_map);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Checkpoint Location");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        latitude = getIntent().getDoubleExtra("latitude", 0.0);
+        longitude = getIntent().getDoubleExtra("longitude", 0.0);
+        title = getIntent().getStringExtra("title");
+        address = getIntent().getStringExtra("address");
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fullMap);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if (latitude != 0.0 || longitude != 0.0) {
+            LatLng checkpointLocation = new LatLng(latitude, longitude);
+
+            mMap.addMarker(new MarkerOptions()
+                    .position(checkpointLocation)
+                    .title(title)
+                    .snippet(address));
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(checkpointLocation, 15f));
+        } else {
+            LatLng toronto = new LatLng(43.6532, -79.3832);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(toronto, 12f));
+        }
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
+}
